d5b6191c9db5188ffa4dbfe38026a2677a1d7a63
aakankk <130693181+aakankk@users.noreply.github.com>
2023-05-27 00:19:25 +0530

    Update process_image.c

--- src/process_image.c
+++ src/process_image.c
@@ -67,7 +67,28 @@ void shift_image(image im, int c, float v)
 
 void clamp_image(image im)
 {
-    
+    for(int c=0; c<im.c; c++)
+    {
+        for(int y=0; y<im.h; y++)
+        {
+            for(int x=0; x<im.w; x++)
+            {
+                float val = get_pixel(im, x, y, c);
+                if(val<0)
+                {
+                    set_pixel(im, x, y, c, 0);
+                }
+                else if(val>1)
+                {
+                    set_pixel(im, x, y, c, 1);
+                }
+                else
+                {
+                    set_pixel(im, x, y, c, val);
+                }
+            }
+        }
+    }
 }
 
 
@@ -84,10 +105,60 @@ float three_way_min(float a, float b, float c)
 
 void rgb_to_hsv(image im)
 {
-    // TODO Fill this in
+    for(int y=0; y<im.h; y++)
+    {
+        for(int x=0; x<im.w; x++)
+        {
+            float r = get_pixel(im, x, y, 0);
+            float g = get_pixel(im, x, y, 1);
+            float b = get_pixel(im, x, y, 2);
+            float V = three_way_max(r, g, b);
+            float m = three_way_max(r, g, b);
+            float C = V-m;
+            float s = C/V;
+            float h1,h2;
+            if(V=0)
+            {
+                s=0;
+            }
+            else
+            {
+                if(V == r)
+                {
+                    h1 = (g-b)/C;
+                }
+                else if(V == g)
+                {
+                    h1 = 2.0 + (b-r)/C;
+                }
+                else
+                {
+                    h1 = 4.0 + (r-g)/C;
+                }
+                if(h1<0)
+                {
+                    h2 = h1/6 + 1;
+                }
+                else
+                {
+                    h2 = h1/6;
+                }
+                set_pixel(im, x, y, 0, h2);
+                set_pixel(im, x, y, 1, s);
+                set_pixel(im, x, y, 2, v);
+            }
+        }
+    }
 }
 
 void hsv_to_rgb(image im)
 {
-    // TODO Fill this in
+    for(int y=0; y<im.h; y++)
+    {
+        for(int x=0; x<im.w; x++)
+        {
+            float h = get_pixel(im, x, y, 0);
+            float s = get_pixel(im, x, y, 1);
+            float v = get_pixel(im, x, y, 2);
+            
 }
